941640158341ecf26dbb2b341075772a322b6b04
Bini960 <castroandrestruco@gmail.com>
2026-03-08 17:33:35 -0600

    Programación clase nodo para manejo de datos y colisiones

--- src/Nodo.java
+++ src/Nodo.java
@@ -1,3 +1,12 @@
 public class Nodo {
-    
+    String llave;       // Aquí se guarda la constelación 
+    String valor;       // Aquí se guarda la estrella
+    Nodo siguiente;     // Atributo para la lista enlazada si hay colisión
+
+    // Constructor: Empaquetado de los datos
+    public Nodo(String llave, String valor) {
+        this.llave = llave;
+        this.valor = valor;
+        this.siguiente = null; // Al crearse la caja no apunta a nada
+    }
 }
